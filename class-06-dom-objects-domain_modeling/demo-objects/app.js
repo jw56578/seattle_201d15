ccd7345fcb4f8107a64ca21d130ed62cb8d79e38
Sam Hamm <samhamm@gmail.com>
2016-09-26 11:24:30 -0700

    uploading lab 9/26

--- class-06-dom-objects-domain_modeling/demo-objects/app.js
+++ class-06-dom-objects-domain_modeling/demo-objects/app.js
@@ -1,57 +1,66 @@
 'use strict';
 
 var myArray = ['a', 'b', 'c'];
-console.log('myArray is', myArray);
+// console.log('myArray is', myArray);
 //
-// var myObject = {
-//   0: 'a',
-//   1: 'b',
-//   2: 'c'
-// };
+var myObject = {
+  0: 'a',
+  1: 'b',
+  2: 'c'
+};
 // console.log('myObject is', myObject);
 //
-// var emptyObject = {};
-// var oneLineObject = { a: 1, b: 2 };
+var emptyObject = {};
+emptyObject.frazier = 'Fraz';
+// console.log(emptyObject);
+
+var oneLineObject = { a: 1, b: 2 };
 // console.log(oneLineObject);
 //
-// var genericObject = {
-//   key1: 'value1',
-//   key2: 'value2',
-//   'multi-word key': 'value3',
-//   method: function() {
-//     //do stuff
-//   }
-// };
+var genericObject = {
+  key1: 'value1',
+  key2: 'value2',
+  'multi-word key': 'value3',
+  method: function() {
+    //do stuff
+    alert('YOU DONE CALLED MY METHOD ERMAGEHRD');
+  }
+};
 //
-// var sam = {
-//   //properties
-//   firstName: 'Sam',
-//   middleName: null,
-//   lastName: 'Hamm',
-//   rating: 0,
-//   isABoss: true,
-//   underlings: ['Rachel', 'Frazier', 'Aliza', 'Maelle'],
+var sam = {
+  //properties
+  firstName: 'Sam',
+  middleName: null,
+  lastName: 'Hamm',
+  rating: 0,
+  isABoss: true,
+  underlings: ['Rachel', 'Frazier', 'Aliza', 'Maelle'],
+
+  //methods
+  getRating: function() {
+    return this.rating;
+  },
+  setRating: function(num) {
+    return this.rating = num;
+  },
+  listUnderlings: function(){
+    for (var i = 0; i < this.underlings.length; i++) {
+      console.log(this.underlings[i]);
+    }
+  }
+ };
+
+sam.employer = {
+  name: 'Code Fellows',
+  location: 'Seattle'
+};
 //
-//   //methods
-//   getRating: function() {
-//     return this.rating;
-//   },
-//   setRating: function(num) {
-//     return this.rating = num;
-//   }
-//  };
-//
-// sam.employer = {
-//   name: 'Code Fellows',
-//   location: 'Seattle'
-// };
-//
-// sam.logName = function() {
-//   console.log(this.firstName + ' ' + this.lastName);
-// };
-//
-// sam.whatIsThis = function() {
-//   console.log(this);
-// };
-//
-// sam.whatIsThis(); //logs the sam object
+sam.logName = function() {
+  console.log(this.firstName + ' ' + this.lastName);
+};
+
+sam.whatIsThis = function() {
+  console.log(this);
+};
+
+sam.whatIsThis(); //logs the sam object
