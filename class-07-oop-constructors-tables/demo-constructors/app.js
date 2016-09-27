412149b35780f4d934543d66d32a5cca6dd422cf
Sam Hamm <samhamm@gmail.com>
2016-09-27 12:38:24 -0700

    end of class 9/27

--- class-07-oop-constructors-tables/demo-constructors/app.js
+++ class-07-oop-constructors-tables/demo-constructors/app.js
@@ -1,18 +1,45 @@
 // Demo for objects-to-constructors
 'use strict';
 
-// var hawa = {
-//   course: '201d15',
-//   firstName: 'Hawa',
-//   lastInitial: 'A',
-//   faveNumber: 13,
-//   isCodeNinja: true,
-//   introduction: function() {
-//     console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
-//   }
-// };
+var hawa = { //eslint-disable-line
+  course: '201d15',
+  firstName: 'Hawa',
+  lastInitial: 'A',
+  faveNumber: 17,
+  isCodeNinja: true,
+  introduction: function() {
+    console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
+  }
+};
 
 // I would need 275 lines of code to model our class this way
 // (25 students * 11 lines each)
 
 // What if... I had a function to make these many similar objects for me? A function where I could pass in a student's unique characteristics as arguments, and then that function would make the individual objects?
+
+// var hawa = {
+function Student(firstName, lastInitial, faveNumber) {
+  // this.course = '201d15';
+  this.firstName = firstName;
+  this.lastInitial = lastInitial;
+  this.faveNumber = faveNumber;
+  this.isCodeNinja = true;
+  this.introduction = function() {
+    console.log('My name is ' + this.firstName + ' and my favorite number is ' + this.faveNumber);
+  };
+  mahClass.push(this);
+  console.log(mahClass);
+  alert('A new student was just added to the class');
+}
+
+var mahClass = [];
+Student.prototype.course = '301d11';
+
+// Instances
+new Student('Hawa', 'A', 17);
+new Student('Marc', 'F', 33);
+new Student('Michael', 'P', 7);
+
+console.log()
+
+// Instead of 275 lines, I can use an object constructor of 11 lines plus 25 one-line instantiations of individual students, and model the entire class with 36 lines.
--- class-07-oop-constructors-tables/demo-tables/app.js
+++ class-07-oop-constructors-tables/demo-tables/app.js
@@ -19,11 +19,11 @@ var toeny = new Cat('Toeny', 'Gray', 'Medium-Large');
 //   console.log(allCats[i].name);
 // }
 
-console.log(bud);
-console.log(alley);
-console.log(trill);
-console.log(meowmix);
-console.log(toeny);
+// console.log(bud);
+// console.log(alley);
+// console.log(trill);
+// console.log(meowmix);
+// console.log(toeny);
 
 /* To see what we need to do to build a HTML table with JS, let's take a look at the HTML needed to start the table and make the top two rows:
 
@@ -46,105 +46,45 @@ Tables are built in the same way that a typewriter types: left-to-right, top-to-
 We'll need to create and append HTML elements in the same order.
 
 We have already put an empty <table> element in the HTML, with id="cats" */
+function catTableJS() {
+  var catTable = document.getElementById('catjs');
+  console.log('catTable is', catTable);
 
+  function makeHeaderRow() {
+    var trEl = document.createElement('tr');
 
+    var thEl = document.createElement('th');
+    thEl.textContent = 'Name';
+    trEl.appendChild(thEl);
 
-var catTable = document.getElementById('catjs');
-console.log('catTable is', catTable);
+    var thEl = document.createElement('th');
+    thEl.textContent = 'Color';
+    trEl.appendChild(thEl);
 
-function makeHeaderRow() {
-  // <tr>              create tr
-  var tableRow = document.createElement('tr');
+    var thEl = document.createElement('th');
+    thEl.textContent = 'Tail';
+    trEl.appendChild(thEl);
 
-  //   <th>Name</th>   create a th, give it content, add it to tr
-  var thElement = document.createElement('th');
-  thElement.textContent = 'Name';
-  tableRow.appendChild(thElement);
-
-  //   <th>Color</th>  create a th, give it content, add it to tr
-  thElement = document.createElement('th');
-  thElement.textContent = 'Color';
-  tableRow.appendChild(thElement);
-
-  //   <th>Tail</th>   create a th, give it content, add it to tr
-  thElement = document.createElement('th');
-  thElement.textContent = 'Tail';
-  tableRow.appendChild(thElement);
-
-  // </tr>             add tr to the table
-  catTable.appendChild(tableRow);
-}
-
-function makeBuddyRow() {
-  // <tr>              create tr
-  var tableRow = document.createElement('tr');
-
-  //   <th>Name</th>   create a th, give it content, add it to tr
-  var tdElement = document.createElement('td');
-  tdElement.textContent = allCats[0].name;
-  tableRow.appendChild(tdElement);
-
-  //   <td>Color</td>  create a td, give it content, add it to tr
-  tdElement = document.createElement('td');
-  tdElement.textContent = allCats[0].color;
-  tableRow.appendChild(tdElement);
-
-  //   <td>Tail</td>   create a td, give it content, add it to tr
-  tdElement = document.createElement('td');
-  tdElement.textContent = allCats[0].tail;
-  tableRow.appendChild(tdElement);
-
-  // </tr>             add tr to the table
-  catTable.appendChild(tableRow);
-}
-
-function makeAlistairRow() {
-  // <tr>              create tr
-  var tableRow = document.createElement('tr');
-
-  //   <th>Name</th>   create a th, give it content, add it to tr
-  var tdElement = document.createElement('td');
-  tdElement.textContent = allCats[1].name;
-  tableRow.appendChild(tdElement);
-
-  //   <td>Color</td>  create a td, give it content, add it to tr
-  tdElement = document.createElement('td');
-  tdElement.textContent = allCats[1].color;
-  tableRow.appendChild(tdElement);
-
-  //   <td>Tail</td>   create a td, give it content, add it to tr
-  tdElement = document.createElement('td');
-  tdElement.textContent = allCats[1].tail;
-  tableRow.appendChild(tdElement);
-
-  // </tr>             add tr to the table
-  catTable.appendChild(tableRow);
-}
-
-function makeAllCatRows() {
-
-  for (var cat = 0; cat < allCats.length; cat++) {
-    // <tr>              create tr
-    var tableRow = document.createElement('tr');
-
-    //   <th>Name</th>   create a th, give it content, add it to tr
-    var tdElement = document.createElement('td');
-    tdElement.textContent = allCats[cat].name;
-    tableRow.appendChild(tdElement);
-
-    //   <td>Color</td>  create a td, give it content, add it to tr
-    tdElement = document.createElement('td');
-    tdElement.textContent = allCats[cat].color;
-    tableRow.appendChild(tdElement);
-
-    //   <td>Tail</td>   create a td, give it content, add it to tr
-    tdElement = document.createElement('td');
-    tdElement.textContent = allCats[cat].tail;
-    tableRow.appendChild(tdElement);
-
-    // </tr>             add tr to the table
+    catTable.appendChild(trEl);
   }
-  catTable.appendChild(tableRow);
-}
 
-makeHeaderRow();
+  makeHeaderRow();
+
+  for (var i = 0; i < allCats.length; i++) {
+    var trEl = document.createElement('tr');
+
+    var tdEl = document.createElement('td');
+    tdEl.textContent = allCats[i].name;
+    trEl.appendChild(tdEl);
+
+    var tdEl = document.createElement('td');
+    tdEl.textContent = allCats[i].color;
+    trEl.appendChild(tdEl);
+
+    var tdEl = document.createElement('td');
+    tdEl.textContent = allCats[i].tail;
+    trEl.appendChild(tdEl);
+
+    catTable.appendChild(trEl)
+  }
+}
